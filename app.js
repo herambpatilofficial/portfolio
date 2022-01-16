9e5daa7af2be472bf82aa8157afdf71dd108afa4
herambpatilofficial <68045041+herambpatilofficial@users.noreply.github.com>
2022-01-16 12:46:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+function contact() {
+    alert("Contact me via:\nPhone number - 8624870960\nEmail id - gdscpvgcoet@pvgcoet.ac.in")
+}
+
+{/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+    function postToGoogle() {
+      var field1 = $("#Name").val();
+      var field2 = $("#Email").val();
+      var field3 = $("#Phone").val();
+      var field4 = $("#Message").val();
+
+      $.ajax({
+        url: "{https://docs.google.com/forms/u/0/d/e/1FAIpQLSdoDLc8L2Fgxukreax-nKQ41-a2H3ADqBv0fPjPfA1PmCZ47Q/formResponse}",
+          
+        //add your google form generated numbers below which are also the 'names' of your inputs     
+        data: {
+          "entry.844511882": field1,
+          "entry.1840624954": field2,
+          "entry.2045696274": field3,
+          "entry.91230418": field4
+        },
+        type: "POST",
+        dataType: "xml",
+        success: function (d) {
+          $('#contact').trigger('reset');
+        },
+        error: function (x, y, z) {
+          $('#contact').trigger('reset');
+        }
+      });
+      return false;
+    } */}
